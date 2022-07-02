4f1a8d59bb7cd3ffbb2d1d94415e9e47104f95b7
Gabriel Cañarte <93744818+GabrielCanarte14@users.noreply.github.com>
2022-07-01 19:22:36 -0500

    correciones

--- proyecto.asm
+++ proyecto.asm
@@ -5,7 +5,7 @@
 #----------------------------------------------
 
 .data
- ingreso: .asciiz "Ingrese monedas: "
+ ingreso: .asciiz "Ingrese su moneda: "
  salto: .asciiz "\n"
  incorrecto: .asciiz "Moneda no valida"
  saldo: .asciiz "Saldo\n"
@@ -20,46 +20,57 @@
  ins: .asciiz "Para si ingrese 1\nPara no ingrese 0\n"
  repetir: .asciiz "Desea ingresar otra moneda?\n"
  buffer: .space 10
+ p: .float 0.12
 .text
 .globl main
 
 main:
 	lwc1 $f4, zeroAsFloat 
 	
+	#imprime isntrucciones
 	jal instrucciones
-	j ingresar
-	j repe
-
-parte2:
+	
+	#Llama al ingreso de monedas
+	jal ingresar
+	
+	#Nueva linea
 	li $v0, 4
-	la $a0, saldo
+	la $a0, salto
 	syscall
 	
+	#Solicita el ingreso del numero de telefono
 	li $v0, 4
 	la $a0, numero
 	syscall
 	
+	#Para leer el numero ingresado, se establece que maximo tenga 10 digitos
 	li $v0, 8
 	la $a0, buffer
 	li $a1, 11
 	move $t1, $a0
 	syscall
 	
+	#Nueva linea
 	li $v0, 4
 	la $a0, salto
 	syscall
 	
+	#Valor por minuto
 	li $v0, 4
 	la $a0, cMinuto
 	syscall
-	#jal random
 	
+	#Generar cuanto va a costar el minuto.
+	#Simular la llamada
+	#calcular el costo
+	#calcular el cambio
+	
+	#Termina el programa
 	j done
 	
 #Solicita el ingreso de monedas
 ingresar:
-
-			
+	
 	li $v0, 4
 	la $a0, ingreso
 	syscall
@@ -67,14 +78,25 @@ ingresar:
 	li $v0, 6
 	syscall
 	
+	add.s $f12, $f12, $f0
+	
 	li $v0, 4
 	la $a0, salto
 	syscall
 	
+	j repe	
+
+#Para regresar a main e imprimir la suma de monedas	
+seguir:	
+	li $v0, 4
+	la $a0, saldo
+	syscall
 	
-	add.s $f12, $f0, $f4
-	
-	jal repe	
+	li $v0, 2
+	add.s $f11, $f12, $f4
+	syscall
+	#Regresas a main linea 31
+	jr $ra
 
 #Pregunta si quiere repetir el ingreso de monedas	
 repe:
@@ -88,7 +110,7 @@ repe:
 	
 	move $t0, $v0
 	
-	beq $t0, $zero, parte2
+	beq $t0, $zero, seguir
 	j ingresar
 	
 		
@@ -99,12 +121,10 @@ instrucciones:
 	li $v0, 4
 	la $a0, ins
 	syscall
+	
+	#Regresas a main linea 30
 	jr $ra
 
-random: 
-        li $a1, 10
-	li $v0, 42
-	syscall
 
 #Finaliza el programa
 done:
